d0ca8ac77e68739968cb935c5d1254f0c0939aae
Ashwin <ashwinlaly@gmail.com>
2020-01-23 19:01:59 +0530

    Cors error correction

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ require('dotenv').config()
 var express = require('express'),
     app = express(),
     db = require('./db'),
+    bodyParser = require('body-parser'),
     userRoute = require('./routes/userRoute')();
 
-app.use(express.json())
-app.use(userRoute);
+app.use(bodyParser.urlencoded({ extended : true }))
+app.use(bodyParser.json())
+app.use(function(req,res,next){
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Headers","*");
+    res.setHeader("Access-Control-Allow-Credentials",false);
+    res.setHeader("Access-Control-Allow-Methods","GET, PUT, PATCH, DELETE, POST");
+    next();
+});
+
+// app.use(express.json())
+app.use(userRoute)
 
 app.all("/", (req, res) => {
     res.status(404).send({
